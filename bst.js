44bc25fa621b7ac52f7886a2aeb4828330038c64
ethanwhall389 <125170834+ethanwhall389@users.noreply.github.com>
2023-12-07 17:15:51 +0000

    Begin add insert function

--- bst.js
+++ bst.js
@@ -14,30 +14,47 @@ class Tree {
     }
 
 
-    buildTree(array) {
-        //find midpoint
-        //create a root node using the midpoint
-        //recursive call to the left using midpoint-1 as end
-        //recursive call to the left using midpoint+1 as start
-        //return the created node
+  buildTree(array) {
+      //find midpoint
+      //create a root node using the midpoint
+      //recursive call to the left using midpoint-1 as end
+      //recursive call to the left using midpoint+1 as start
+      //return the created node
 
-        // const array = sortArray(unsortedArray);
+      // const array = sortArray(unsortedArray);
 
 
-        const start = 0;
-        const end = array.length - 1;
+      const start = 0;
+      const end = array.length - 1;
 
-        if (start > end) return null;
+      if (start > end) return null;
 
-        const midpoint = Math.floor((start + end) / 2);
-        const rootNode = new Node(array[midpoint]);
+      const midpoint = Math.floor((start + end) / 2);
+      const rootNode = new Node(array[midpoint]);
 
-        const leftArray = array.slice(0, midpoint);
-        const rightArray = array.slice(midpoint+1);
-        rootNode.leftChild = this.buildTree(leftArray);
-        rootNode.rightChild = this.buildTree(rightArray);
-        return rootNode;
+      const leftArray = array.slice(0, midpoint);
+      const rightArray = array.slice(midpoint+1);
+      rootNode.leftChild = this.buildTree(leftArray);
+      rootNode.rightChild = this.buildTree(rightArray);
+      return rootNode;
+  }
+
+  insert(value) {
+    console.log('running insert')
+    console.log(value);
+    let temp = this.root;
+    while (temp !== null) {
+      if (value > temp) {
+        temp = temp.rightChild;
+      } else {
+        temp = temp.leftChild;
+      }
     }
+    temp = new Node(value);
+    console.log(temp);
+    console.log(temp.leftChild);
+  }
+
 }
 
 
@@ -91,5 +108,11 @@ const array = [1, 4, 2, 5, 9, 6, 3, 5];
 
 const bst = new Tree(array);
 
+
 prettyPrint(bst.root);
 
+bst.insert(8);
+
+prettyPrint(bst.root);
+
+console.log(bst);
